2e4c83fc9df6fcf6f835ebdd0159cfd23b311db2
dvsidorov <dvsidorov88@mail.ru>
2019-09-29 12:52:48 +0500

    add_event

--- backend/ml/prediction.py
+++ backend/ml/prediction.py
@@ -1,51 +1,48 @@
 # coding: utf-8
 
+import pandas as pd
+import pandas
+import pprint
+from gensim.models import word2vec
+import codecs
+import numpy as np
+import codecs
+import shutil
+import os
+import pymorphy2
+import seaborn as sns
+from pandas import read_csv
+import re
+import gensim.downloader as api
 
-def load_ml():
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import normalize
+import numpy as np
 
-    import pandas as pd
-    import pandas
-    import pprint
-    from gensim.models import word2vec
-    import codecs
-    import numpy as np
-    import codecs
-    import shutil
-    import os
-    import pymorphy2
-    import seaborn as sns
-    from pandas import read_csv
-    import re
-    import gensim.downloader as api
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
 
-    from sklearn.cluster import KMeans
-    from sklearn.manifold import TSNE
-    from sklearn.preprocessing import normalize
-    import numpy as np
+from matplotlib import pyplot as plt
 
-    from sklearn.cluster import DBSCAN
-    from sklearn import metrics
-    from sklearn.datasets.samples_generator import make_blobs
-    from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
-    from matplotlib import pyplot as plt
+from sklearn.cluster import MeanShift
+import json
 
-    from sklearn.decomposition import PCA
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
 
-    from sklearn.cluster import MeanShift
-    import json
+from sklearn.utils import shuffle
 
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import seaborn as sns
-    from sklearn.linear_model import LogisticRegression
-
-    from sklearn.utils import shuffle
-
-    from sklearn.linear_model import LogisticRegression
-    from sklearn import metrics
-    from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
 
 
-
+def
